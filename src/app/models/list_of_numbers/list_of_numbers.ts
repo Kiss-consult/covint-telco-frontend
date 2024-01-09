d1f0f1fbb5b9b1233af27bff9f8f119c753baa2f
saricsacsi <saricsacsi@gmail.com>
2024-01-09 16:45:29 +0100

    kampány fromtime totime formátum, telefonlista feltoltes névvel egyebek

---
+++ src/app/models/list_of_numbers/list_of_numbers.ts
@@ -0,0 +1,12 @@
+
+import { Number } from "./number";
+
+
+
+export class List_of_numbers {
+    id: number = 0;
+    name: string = "";
+    timestamp: string = "";
+    numbers: Number[] = [];
+
+}
