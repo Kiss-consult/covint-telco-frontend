f0e95b9d0272feae282d9b797a3e4fc4c08003f0
saricsacsi <saricsacsi@gmail.com>
2024-01-11 08:48:37 +0100

    kampány keresés

--- src/app/components/pages/managecampaign/managecampaign.component.ts
+++ src/app/components/pages/managecampaign/managecampaign.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import {MatSort, Sort} from '@angular/material/sort';
+import { MatSort, Sort } from '@angular/material/sort';
 import { ResultCampaign } from 'src/app/models/result_campaign/result_campaign';
 import { BackendService } from 'src/app/services/backend/backend.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { Location } from '@angular/common'
+import { Campaign } from 'src/app/models/campaign/campaign';
 
 @Component({
   selector: 'app-managecampaign',
@@ -15,33 +16,31 @@ export class ManagecampaignComponent {
 
 
 
-resultCampaing :ResultCampaign = new ResultCampaign;
-resultCampaings: ResultCampaign[] = [];
-
-
-  dataSource!: MatTableDataSource<ResultCampaign>; 
-
-
+  resultCampaing: ResultCampaign = new ResultCampaign;
+  resultCampaings: ResultCampaign[] = [];
+  dataSource!: MatTableDataSource<ResultCampaign>;
   @ViewChild('paginator')
   paginator!: MatPaginator;
-@ViewChild('empTbSort') empTbSort = new MatSort();
-pageSizeOptions: number[] = [5, 10];
+  @ViewChild('empTbSort') empTbSort = new MatSort();
+  pageSizeOptions: number[] = [5, 10];
 
-displayedColumns: string[] = ['name','startDate','endDate','liveOrAuto','numberOfQuestions','VpbxUuid','started']; 
+  displayedColumns: string[] = ['name', 'startDate', 'endDate', 'liveOrAuto', 'numberOfQuestions', 'VpbxUuid', 'started'];
 
-Filter(event: Event) {
-  const filterValue = (event.target as HTMLInputElement).value;
-  this.dataSource.filter = filterValue.trim().toLowerCase();
-}
+  campaign: Campaign = new Campaign;
 
-goBackToPrevPage(): void {
-  this.location.back();
-}
-  constructor(private backendService: BackendService,private location: Location ) {
+  Filter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  goBackToPrevPage(): void {
+    this.location.back();
+  }
+  constructor(private backendService: BackendService, private location: Location) {
 
 
-      
-    this.backendService. getAllCampaign().subscribe(
+
+    this.backendService.getAllCampaign().subscribe(
       result => {
         if (result.isErr()) {
           alert("Kampányok sikertelen betöltés");
@@ -52,21 +51,33 @@ goBackToPrevPage(): void {
         this.dataSource = new MatTableDataSource(this.resultCampaings);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.empTbSort;
-  
-       // this.dataSource.data = this.illnesses; // Az adatforrás frissítése
+
+        // this.dataSource.data = this.illnesses; // Az adatforrás frissítése
         console.log("Kampányok sikeres betöltés");
         console.log(this.resultCampaings);
-        
-
-        
-          
-
       });
-      
+
   }
 
 
+public searchCampaign() {
+  this.backendService.searchCampaign(this.campaign.name, this.campaign.startDate, this.campaign.endDate).subscribe(
+    result => {
+      if (result.isErr()) {
+        alert("Kampányok keresése sikertelen betöltés");
+        console.error(result.unwrapErr());
+        return;
+      }
+      this.resultCampaings = result.unwrap();
+      this.dataSource = new MatTableDataSource(this.resultCampaings);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.empTbSort;
 
+      this.dataSource.data = this.resultCampaings; // Az adatforrás frissítése
+      console.log("Kampányok keresése sikeres betöltés");
+      console.log(this.resultCampaings);
+    });
+}
 
 
 
