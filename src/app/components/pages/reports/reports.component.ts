63ec079bf5806778fa59a659baeb374d609c6624
saricsacsi <saricsacsi@gmail.com>
2024-02-04 21:59:54 +0100

    eredményoldal_kampány keresés

--- src/app/components/pages/reports/reports.component.ts
+++ src/app/components/pages/reports/reports.component.ts
@@ -12,11 +12,12 @@ import { Filter } from 'src/app/models/filter/filter';
 
 import { LoginService } from 'src/app/services/login/login.service';
 
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/internal/Observable';
 
 import { Location } from '@angular/common'
 import { ResultCampaign } from 'src/app/models/result_campaign/result_campaign';
+import { Campaign } from 'src/app/models/campaign/campaign';
 
 @Component({
   selector: 'app-reports',
@@ -25,40 +26,77 @@ import { ResultCampaign } from 'src/app/models/result_campaign/result_campaign';
 })
 export class ReportsComponent {
 
-  displayedColumns: string[] = ['name','id']; // Itt adhatod meg az oszlopok neveit
-  dataSource: MatTableDataSource<ResultCampaign>;
-
-  @ViewChild('paginator')  paginator: MatPaginator; /// erre figyelni majd
+  resultCampaing: ResultCampaign = new ResultCampaign;
+  resultCampaings: ResultCampaign[] = [];
+  dataSource!: MatTableDataSource<ResultCampaign>;
+  @ViewChild('paginator')
+  paginator!: MatPaginator;
+  @ViewChild('empTbSort') empTbSort = new MatSort();
   pageSizeOptions: number[] = [5, 10];
-  resultCampaigns:ResultCampaign[] = [];
-  resultCampaign: ResultCampaign = new ResultCampaign;
+
+  displayedColumns: string[] = ['name', 'startDate', 'endDate', 'liveOrAuto', 'numberOfQuestions', 'VpbxUuid','id', 'started','campaign'];
+
+  campaign: Campaign = new Campaign;
 
   Filter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
+
   goBackToPrevPage(): void {
     this.location.back();
   }
-
-  constructor(private backendService: BackendService, public loginService: LoginService, private router: Router,  
-    private changeDetector: ChangeDetectorRef,private location: Location){
+  constructor(private backendService: BackendService, private location: Location, private router: Router,private _Activatedroute:ActivatedRoute) {
 
 
-      this.backendService.getAllCampaign().subscribe(
-        result => {
-          if (result.isErr()) {
-            alert("Betegségek lekérdezése sikertelen");
-            console.error(result.unwrapErr());
-            return;
-          }
-          this.resultCampaigns = result.unwrap();
-  
-          console.log("Sikeresen lekérdezve a betegségek az adatbázisból");
-          console.log(this.resultCampaigns);
-          this.dataSource = new MatTableDataSource(this.resultCampaigns);
-          this.dataSource.paginator = this.paginator;
-  
-        });
-    }
+
+    this.backendService.getAllCampaign().subscribe(
+      result => {
+        if (result.isErr()) {
+          alert("Kampányok sikertelen betöltés");
+          console.error(result.unwrapErr());
+          return;
+        }
+        this.resultCampaings = result.unwrap();
+        this.dataSource = new MatTableDataSource(this.resultCampaings);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.empTbSort;
+
+        // this.dataSource.data = this.illnesses; // Az adatforrás frissítése
+        console.log("Kampányok sikeres betöltés");
+        console.log(this.resultCampaings);
+      });
+
+  }
+
+
+public searchCampaign() {
+  this.backendService.searchCampaign(this.campaign.name, this.campaign.startDate, this.campaign.endDate).subscribe(
+    result => {
+      if (result.isErr()) {
+        alert("Kampányok keresése sikertelen betöltés");
+        console.error(result.unwrapErr());
+        return;
+      }
+      this.resultCampaings = result.unwrap();
+      this.dataSource = new MatTableDataSource(this.resultCampaings);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.empTbSort;
+
+      this.dataSource.data = this.resultCampaings; // Az adatforrás frissítése
+      console.log("Kampányok keresése sikeres betöltés");
+      console.log(this.resultCampaings);
+    });
+}
+
+public callCampaignResult(campaign: ResultCampaign){
+  console.log("result campaign",campaign)
+  console.log("id",campaign.id)
+   let id = campaign.id;
+    this.router.navigate(
+        ['/campaign', id]
+    ); 
+}
+
+
 }
